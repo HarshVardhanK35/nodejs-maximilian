e68e9a2a3c405f89d0399d8bfbdd0f80575496aa
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-08-05 22:11:43 +0530

    parsing requests, using express router, serving HTML pages

--- main/app.js
+++ main/app.js
@@ -1,26 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+// import modules
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express()
 
-// app.use((req, res, next) => {
-//   console.log('first middleware')
-//   next()
-// })
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// app.use((req, res, next) => {
-//   console.log('second middleware')
-//   res.send('<p>This is express page</p>')
-// })
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-app.use('/users', (req, res, next) => {
-  console.log('middleware that runs only with /users route')
-  res.send("<h3>this middlewares that handles only '/users' route</h3>")
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404</h1><h3>Page not found!</h3>')
 })
 
-app.use('/', (req, res, next) => {
-  console.log('middleware that always runs')
-  res.send("<h3>this middlewares that handles only 'home' page</h3>")
-
+app.listen(3000, () => {
+  console.log(`Server is up and running on: http://localhost:3000`)
 })
-
-app.listen(3000)
