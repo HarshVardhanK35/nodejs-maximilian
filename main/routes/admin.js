be4544f8642a93eba523a3d04ad5a63435463cb0
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-08-07 11:08:16 +0530

    styles added

--- main/routes/admin.js
+++ main/routes/admin.js
@@ -3,9 +3,12 @@ const express = require('express')
 
 const router = express.Router()
 
+// root directory
+const rootDir = require('../utils/path')
+
 // ... /admin/add-product => GET req
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 // ... /admin/product => POST req
