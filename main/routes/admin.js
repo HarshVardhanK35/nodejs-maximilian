d781aaf09f78d54f286b80bbd1a7522b16a7771a
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-08-10 20:50:29 +0530

    adding dynamic content via pug

--- main/routes/admin.js
+++ main/routes/admin.js
@@ -7,7 +7,7 @@ const products = []
 
 // ... /admin/add-product => GET req
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.render('add-product', {docTitle: "Add Product"})
 })
 
 // ... /admin/product => POST req
