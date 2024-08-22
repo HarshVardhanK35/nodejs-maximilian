d3ceb8fe09bdeedbde6d46c1aaf7811e97d88a37
HarshVardhanK35 <kasavardhan2001@gmail.com>
2024-08-22 14:46:13 +0530

    started advanced_objects

--- OOPs_max/main/script.js
+++ OOPs_max/main/script.js
@@ -1,8 +1,48 @@
-const per1 = {name: 'har'}
-const per2 = {name: 'var'}
-const personData = new Map([[per1, [{age: 30}]]])
-personData.set(per2, [{ age: 29 }])
+const addMovieButton = document.getElementById('add-movie-btn')
+const searchMovieButton = document.getElementById('search-btn')
 
-for(let entry of personData.entries()) {
-  console.log(entry)
+const movies = []
+
+const renderMovieList = () => {
+
+  const movieList = document.getElementById('movie-list')
+
+  if(movies.length === 0) {
+    movieList.classList.remove('visible')
+    return
+  }
+  else{
+    movieList.classList.add('visible')
+  }
+  movieList.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement('li')
+    movieEl.textContent = movie.info.title
+    movieList.append(movieEl)
+  })
 }
+
+const addMovieHandler = () =>  {
+  const title = document.getElementById('title').value
+  const extraName = document.getElementById('extra-name').value
+  const extraValue = document.getElementById('extra-value').value
+
+  if(title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+    return
+  }
+
+  const newMovie = {
+    info: {
+      title: title,
+      [extraName]: extraValue,
+    },
+    id: Math.random()
+  }
+
+  movies.push(newMovie)
+  console.log(movies)
+  renderMovieList()
+}
+
+addMovieButton.addEventListener('click', addMovieHandler)
